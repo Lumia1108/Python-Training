3637a862f59b2e22cff1b4f9014d3b95c640b979
JH <jhsoft1108@gmail.com>
2021-03-08 13:59:25 +0900

    텍스트 가공 연습 (2)

--- script/script.py
+++ script/script.py
@@ -14,11 +14,11 @@ monica = ""
 for item in Line:
     print(item)
     monica += (item + '\n')
-'''
+
 f = codecs.open('monica.txt', 'w', encoding='utf-8')
 f.write(monica)
 f.close()
-'''
+
 
 # 등장인물 리스트 만들기
 characters = re.findall(r'[A-Z][a-z]+:', script101)
@@ -33,3 +33,23 @@ script = re.findall(r'\([A-Za-z].+[a-z|\.]\)', script101)
 for item in script:
     print(item)
 
+# 특정 단어의 예문만 모아 파일로 저장하기
+## 대사만 모으기
+f = codecs.open('friends101.txt', 'r', encoding='utf-8')
+sentences = f.readlines()
+script2 = []
+for item in sentences:
+    if re.match(r'[A-Z][a-z]+:', item):
+        script2.append(item)
+        print(item)
+
+## would가 들어간 문장만 모으기
+would = []
+for i in script2:
+    if re.search('would', i):
+        would.append(i)
+        print(i)
+
+f = codecs.open('would.txt', 'w', encoding='utf-8')
+f.writelines(would)
+f.close()
